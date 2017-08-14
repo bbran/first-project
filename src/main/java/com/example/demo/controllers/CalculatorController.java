ede6f8f86c4af8277a8bf781549b60e784d006b0
Ben Brandvig <benjamin.brandvig@libertymutual.com>
2017-08-14 09:08:12 -0700

    Added calculation history.

--- src/main/java/com/example/demo/controllers/CalculatorController.java
+++ src/main/java/com/example/demo/controllers/CalculatorController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.example.demo.models.Adder;
 import com.example.demo.models.Divider;
 import com.example.demo.models.Exponentiator;
+import com.example.demo.models.HistoryEntry;
 import com.example.demo.models.Modulotor;
 import com.example.demo.models.Multiplier;
 import com.example.demo.models.Subtractor;
@@ -16,14 +21,22 @@ import com.example.demo.models.Subtractor;
 @Controller
 @RequestMapping("/calculator")
 public class CalculatorController {
+	List<HistoryEntry> historyList = new ArrayList<>();
+	
+	@ModelAttribute("history")
+	public Iterable<HistoryEntry> history()	{
+		return historyList;
+	}
 	
 	@GetMapping("")
 	public String showCalculator() {
+		historyList.clear();
 		return "calculator/calculator";
 	}
 	
 	@GetMapping("calculate")
 	public String redirectToCalculator()	{
+		historyList.clear();
 		return "calculator/calculator";	
 	}
 	
@@ -33,28 +46,41 @@ public class CalculatorController {
 		
 		if (operation.equals("add"))	{
 			Adder adder = new Adder(left, right);
-			mv.addObject("result", adder.calculate());
+			double theResult = adder.calculate();
+			mv.addObject("result", theResult);
+			historyList.add(0, new HistoryEntry(left + " + " + right + " = " + theResult));
 		}
 		else if (operation.equals("subtract"))	{
 			Subtractor subtractor = new Subtractor(left, right);
-			mv.addObject("result", subtractor.calculate());
+			double theResult = subtractor.calculate();
+			mv.addObject("result", theResult);
+			historyList.add(0, new HistoryEntry(left + " - " + right + " = " + theResult));
 		}
 		else if (operation.equals("multiply"))	{
 			Multiplier multiplier = new Multiplier(left, right);
-			mv.addObject("result", multiplier.calculate());
+			double theResult = multiplier.calculate();
+			mv.addObject("result", theResult);
+			historyList.add(0, new HistoryEntry(left + " * " + right + " = " + theResult));
 		}
 		else if (operation.equals("divide"))	{
 			Divider divider = new Divider(left, right);
-			mv.addObject("result", divider.calculate());
+			double theResult = divider.calculate();
+			mv.addObject("result", theResult);
+			historyList.add(0, new HistoryEntry(left + " / " + right + " = " + theResult));
 		}
 		else if (operation.equals("modulo"))	{
 			Modulotor mod = new Modulotor(left, right);
-			mv.addObject("result", mod.calculate());
+			double theResult = mod.calculate();
+			mv.addObject("result", theResult);
+			historyList.add(0, new HistoryEntry(left + " % " + right + " = " + theResult));
 		}
 		else	{
 			Exponentiator exp = new Exponentiator(left, right);
-			mv.addObject("result", exp.calculate());
+			double theResult = exp.calculate();
+			mv.addObject("result", theResult);
+			historyList.add(0, new HistoryEntry(left + " ^ " + right + " = " + theResult));
 		}
+		mv.addObject("history", historyList);
 		return mv;
 	}
 
---
+++ src/main/java/com/example/demo/models/HistoryEntry.java
@@ -0,0 +1,9 @@
+package com.example.demo.models;
+
+public class HistoryEntry {
+	private String entryDetail;
+	
+	public HistoryEntry(String entry)	{
+		this.entryDetail = entry;
+	}
+}
